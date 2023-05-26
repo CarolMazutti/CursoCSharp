0c73f58ff034f623353c3ff0e4306d63d10efc15
Carol Mazutti <cpmazutti@gmail.com>
2023-05-26 17:29:45 -0300

    aula02 e aula03

---
+++ Aula02/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Aula02{
+    class Program{
+        static void Main(string[] args){
+
+            Console.WriteLine("Carol");
+            if(args.GetLongLength(0)>0){
+                Console.WriteLine(args.GetValue(0));
+            }
+
+            {
+                
+            }
+        }
+    }
+}
